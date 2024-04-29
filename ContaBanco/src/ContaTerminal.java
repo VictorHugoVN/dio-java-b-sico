c707ad4412d51916ce8ea074429d010e4d31fc9a
VictorHugoVN <victorneivadef8@gmail.com>
2024-04-29 16:03:40 -0300

    projeto ContaTerminal finalizada

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal{
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Por favor, informe o número da agência: ");
+        int numero = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Agência: ");
+        String agencia = sc.nextLine();
+        System.out.println("Nome do(a) cliente: ");
+        String nomeCliente = sc.nextLine();
+        System.out.println("Saldo: R$");
+        Double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+        + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque");
+
     }
 }
