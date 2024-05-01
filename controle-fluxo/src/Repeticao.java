c8084052ac4602e677ea99bb3cb5e0ab9820d99d
VictorHugoVN <victorneivadef8@gmail.com>
2024-05-01 10:32:33 -0300

    Controle de fluxo

---
+++ controle-fluxo/src/Repeticao.java
@@ -0,0 +1,9 @@
+public class Repeticao {
+    public static void main(String[] args){
+        String nomes[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+        for(String nomeAtual: nomes){
+            System.out.println(nomeAtual);
+        }
+    }
+    
+}
